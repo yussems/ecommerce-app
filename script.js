7c48f1ccb4c4bb6215413fb177e902b4dbbc03e2
yussems <yussems@gmail.com>
2022-06-20 20:39:42 +0300

    auth fix footer was handled

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ const firstVisit = document.querySelector(".first-Visit");
 const formVal = document.querySelector(".formVal");
 const localInput = document.querySelector(".localInput");
 const userName = document.querySelector(".userName");
+const unsubit = document.querySelector(".unsubit");
+const logout = document.querySelector(".logout");
 
-formVal.addEventListener("submit", (e) => {
-    e.preventDefault();
-  const name = localInput.value;
-  localStorage.setItem("name", name);
-  firstVisit.style.display = "none";
-   window.location.reload();
+//exit from the site
+logout.addEventListener("click", () => {
+  sessionStorage.removeItem("name");
+  window.location.reload();
 });
 
-function checkVisiter() {
-    if (localStorage.getItem("name") === null) {
-      firstVisit.classList.add('d-block')
-    }else{
-        
-        firstVisit.classList.add('d-none')
-    }
 
-}
-checkVisiter()
-
+//check the user
 function setName() {
-    if(localStorage.getItem("name") === ''){
-        userName.innerHTML = 'Ziyaretçi'
+  if (
+    sessionStorage.getItem("name") === "" ||
+    sessionStorage.getItem("name") === null
+  ) {
+    firstVisit.classList.add("d-none");
+    userName.innerHTML = "Ziyaretçi";
+  }
+    if (sessionStorage.getItem("name") !== "") {
+    firstVisit.classList.add("d-none");
+
+        const name = sessionStorage.getItem("name");
+        userName.innerHTML = `Merhaba ${name.toUpperCase()}`;
     }
-    const name = localStorage.getItem("name");
-    userName.innerHTML = `Merhaba ${name.toUpperCase()}`;
+
 }
-setName()
+
+//validation of the form
+formVal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = localInput.value;
+  sessionStorage.setItem("name", name);
+  setName();
+
+});
+
+
+
+unsubit.addEventListener("click", () => {
+  sessionStorage.setItem("name", "");
+  setName()
+});
+
+
